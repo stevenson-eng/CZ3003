ac3a82730a2c4f55a0ed09304fbd6de6934dd407
Tan Wei Lun <weilunn.97@gmail.com>
2021-09-30 19:51:30 +0800

    Enforced FK constraints by default on startup

--- admin_ui/src/main.py
+++ admin_ui/src/main.py
@@ -4,6 +4,17 @@ import models
 from api.api import api_router
 from db.database import engine
 
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+
 app = FastAPI()
 app.include_router(api_router)
 models.Base.metadata.create_all(bind=engine)
