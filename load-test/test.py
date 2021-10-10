301aa992a7c5f551b5b87b7f20865b9fd64011f7
ernest <erne0009@e.ntu.edu.sg>
2021-10-10 14:32:59 +0800

    started on creating student

--- load-test/test.py
+++ load-test/test.py
@@ -8,6 +8,23 @@ engine = Engine()
 
 base_url = "https://0kftyn.deta.dev"
 
+def generate_url(path)
+    return base_url + path
+
+@scenario(engine)
+def create_user_for_load_test(context):
+    response = requests.post(
+        url=generate_url('/student/'),
+        json={
+          "email": "loadtest@gmail.com",
+          "name": "loadtest",
+          "points": 0,
+          "password": "loadtest"
+        },
+    )
+    assert response.status_code == 200
+
+
 @scenario(engine)
 @load_model(n_seconds(180, 30))
 @user_loop(iterations_per_second_limited(4))
