4dde33bba970be570dda20c618ae3c302b17cb79
haozhi98 <haozhi@onextech.com>
2021-10-13 13:35:30 +0800

    added leaderboard limit param

--- admin_ui/src/api/endpoints/leaderboard.py
+++ admin_ui/src/api/endpoints/leaderboard.py
@@ -4,10 +4,10 @@ from db.database import get_db
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 
 router = APIRouter()
 
 @router.get("/", response_model=List[schemas.Student])
-def read(db: Session = Depends(get_db)):
-    return crud.student.getLeaderboard(db)
+def read(db: Session = Depends(get_db), limit: Optional[int] = 10 ):
+    return crud.student.getLeaderboard(db, limit)
