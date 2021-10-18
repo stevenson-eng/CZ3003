fd41721d42093c1d571856084c4cb226d1a4150f
Tan Wei Lun <wtan132@azure-wtan132.duyzjoshnxie3mkpkow2jtipog.ix.internal.cloudapp.net>
2021-10-18 03:27:07 +0000

    Modified GET on attempt

--- admin_ui/src/api/endpoints/attempt.py
+++ admin_ui/src/api/endpoints/attempt.py
@@ -3,19 +3,24 @@ import schemas
 from db.database import get_db
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
 
 router = APIRouter()
 
 
-@router.get("/", response_model=schemas.Attempt)
-def read(quest_name: str, student_email: str, db: Session = Depends(get_db)):
-    return crud.attempt.read(db, quest_name, student_email)
-
-
 @router.get("/", response_model=List[schemas.Attempt])
-def read_all(db: Session = Depends(get_db)):
-    return crud.attempt.read_all(db)
+def read(
+    quest_name: Optional[str] = None,
+    student_email: Optional[str] = None,
+    db: Session = Depends(get_db),
+):
+    """
+    Reads a list of all attempts:
+
+    - **quest_name**: an optional quest name to filter by. If not provided, no filtering is done.
+    - **student_email**: an optional student email to filter by. If not provided, no filtering is done.
+    """
+    return crud.attempt.read(db, quest_name, student_email)
 
 
 @router.post("/")
