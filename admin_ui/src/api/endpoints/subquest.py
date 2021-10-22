7f084f0a4dffee05d3dea1c1c62e415a03d3583f
phlyy88 <73178036+phlyy88@users.noreply.github.com>
2021-10-22 22:33:23 +0800

    Added docstrings for Teacher, Assignment, AssignmentQuestion, Subquest, NPC

--- admin_ui/src/api/endpoints/subquest.py
+++ admin_ui/src/api/endpoints/subquest.py
@@ -10,17 +10,37 @@ router = APIRouter()
 
 @router.get("/{subquest_name}", response_model=schemas.Subquest)
 def read(subquest_name: str, db: Session = Depends(get_db)):
+    """
+    Read specific subquest with following parameters:
+
+    - **subquest_name**: required, existing subquest name
+    """
     return crud.subquest.read(db, subquest_name)
 
 @router.get("/", response_model=List[schemas.Subquest])
 def read_all(db: Session = Depends(get_db)):
+    """
+    Read all subquest, no parameters required
+    """
     return crud.subquest.read_all(db)
 
 @router.post("/")
 def create(subquest: schemas.SubquestCreate, db: Session = Depends(get_db)):
+    """
+    Create a subquest with all the information:
+
+    - **subquest_name**: required, unique name for subquest
+    - **quest_name**: required, existing quest name
+    """
     return crud.subquest.create(db, subquest)
 
 
 @router.patch("/")
 def update(subquest: schemas.SubquestUpdate, db: Session = Depends(get_db)):
+    """
+    Update a subquest with all the information:
+
+    - **subquest_name**: required, unique name for subquest (can be same as before update)
+    - **quest_name**: required, existing quest name (for identification of subquest to update)
+    """
     return crud.subquest.update(db, subquest)
