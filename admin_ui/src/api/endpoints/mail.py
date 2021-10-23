79f485e99e53ef53bec1abdcf342009ab7ba53e9
Tan Wei Lun <weilunn.97@gmail.com>
2021-10-23 14:52:28 +0800

    Added docs for /challenge/ and /mail/

--- admin_ui/src/api/endpoints/mail.py
+++ admin_ui/src/api/endpoints/mail.py
@@ -13,16 +13,35 @@ router = APIRouter()
 
 @router.get("/{id}", response_model=schemas.Mail)
 def read(id: str, db: Session = Depends(get_db)):
+    """
+    Read a single Mail based on its ID.
+
+    - **id**: required, ID of the mail to be read
+    """
     return crud.mail.read(db, id)
 
 
 @router.get("/", response_model=List[schemas.Mail])
 def read_all(db: Session = Depends(get_db)):
+    """
+    Reads a list of all mails that have been sent
+    """
     return crud.mail.read_all(db)
 
 
 @router.post("/")
 async def send_async(email: schemas.MailCreate, db: Session = Depends(get_db)):
+    """
+    Read a list of challenges based on the following optional filters.
+    If the field is not provided, it will not be used for filtering
+
+    - **subject**: required, the email subject
+    - **body_heading**: required, the heading of the email body (presented in a banner)
+    - **body_text**: required, the content of the email body (presented below the heading)
+    - **sender**: required, email address of the sender (must be a teacher)
+    - **recipients**: required, list of email addresses of the recipients (must be students)
+    """
+
     crud.mail.create(db, email)
     await send_mail_async(
         email.subject, email.recipients, email.body_heading, email.body_text
