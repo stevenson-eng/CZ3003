1f9f3087528610f821d97bdb6f9639327b7526d3
haozhi98 <57322708+haozhi98@users.noreply.github.com>
2021-10-25 22:03:19 +0800

    merge main (#60)

--- admin_ui/src/api/endpoints/attempt.py
+++ admin_ui/src/api/endpoints/attempt.py
@@ -26,14 +26,31 @@ def read(
 def student_stats(
     student_email: Optional[str] = None, 
     db: Session = Depends(get_db)):
+    """
+    Reads a list of students with total points earned and max points earnable:
+
+    - **student_email**: an optional student email to filter by. If not provided, no filtering is done.
+    """
     return crud.attempt.read_student_stats(db, student_email)
 
 
 @router.post("/")
 def create(attempt: schemas.AttemptCreate, db: Session = Depends(get_db)):
+    """
+    Create an attempt with all the information:
+    - **quest_name**: required, quest which which this challenge is linked to (e.g. "CZ3001 Quest 1")
+    - **student_email**: required, email address of student which the attempt belongs to
+    - **points_scored**: required, points earned in the attempt
+    - **total_points**: required, maximum points earnable in the attempt
+    - **time_to_complete_in_seconds**: required, time taken for the attempt in seconds
+    - **completion_datetime**: required, datetime of attempt completion in the format, "%Y-%m-%dT%H:%m:%S.%f"
+    """
     return crud.attempt.create(db, attempt)
 
 
 @router.patch("/")
 def update(attempt: schemas.AttemptUpdate, db: Session = Depends(get_db)):
+    """
+    (Not Used)
+    """
     return crud.attempt.update(db, attempt)
--- admin_ui/src/api/endpoints/student.py
+++ admin_ui/src/api/endpoints/student.py
@@ -10,11 +10,18 @@ router = APIRouter()
 
 @router.get("/{email}", response_model=schemas.Student)
 def read(email: str, db: Session = Depends(get_db)):
+    """
+    Read specific assignment question with following parameters:
+    - **email**: required, get from dummy_data file for testing
+    """
     return crud.student.read(db, email)
 
 
 @router.get("/", response_model=List[schemas.Student])
 def read_all(db: Session = Depends(get_db)):
+    """
+    Read all a list of all students, no parameters required
+    """
     return crud.student.read_all(db)
 
 @router.post("/")
@@ -22,8 +29,8 @@ def create(student: schemas.StudentCreate, db: Session = Depends(get_db)):
     """
     Create a student with all the information:
 
-    - **email**: required
-    - **name**: required
+    - **email**: required, student email
+    - **name**: required, student name
     - **points**: optional, default value = 0
     - **status**: optional, default value = 2, which is "offline"
     - **rank**: do not input, default value = None, rank will be calculated from points
@@ -34,6 +41,16 @@ def create(student: schemas.StudentCreate, db: Session = Depends(get_db)):
 
 @router.patch("/")
 def update(student: schemas.StudentUpdate, db: Session = Depends(get_db)):
+    """
+    Update a student with all the information:
+
+    - **email**: required, existing student email
+    - **status**: optional
+    - **name**: optional
+    - **points**: optional
+    - **rank**: do not input, rank will be updated based on points
+    - **position**: do not input, leaderboard will be updated based on points
+    """
     return crud.student.update(db, student)
 
 
